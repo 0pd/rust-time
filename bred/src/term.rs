9f03f8f706c5e5f9a90eb2481aec05c9c6ac107d
Nikita Kamensky <me@nkamensky.com>
2019-03-05 17:53:17 +0300

    Fix substitution

--- bred/src/term.rs
+++ bred/src/term.rs
@@ -43,7 +43,7 @@ fn shift(d: u32, c: u32, term: &Term) -> Term {
 fn substitute(var: u32, arg: &Term, function: &Term) -> Term {
     match function {
         Term::App(f, a) => Term::App(Box::new(substitute(var, arg, f)), Box::new(substitute(var, arg, a))),
-        Term::Abs(v, f) => Term::Abs(*v, Box::new(substitute(var + 1, &shift(1, 0, arg), f))),
+        Term::Abs(v, f) => Term::Abs(*v, Box::new(substitute(var, &shift(1, 0, arg), f))),
         Term::Var(v) if *v == var => arg.clone(),
         Term::Var(v) => function.clone()
     }
@@ -98,61 +98,67 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_reduce_var_normal() {
+    fn test_normal_form_var_normal() {
         let var = Term::Var(0);
         assert_eq!(var, normal_form(&var, Strategy::Normal));
     }
 
     #[test]
-    fn test_reduce_var_applicative() {
+    fn test_normal_form_var_applicative() {
         let var = Term::Var(0);
         assert_eq!(var, normal_form(&var, Strategy::Applicative));
     }
 
     #[test]
-    fn test_reduce_id_normal() {
+    fn test_normal_form_id_normal() {
         let id = Term::Abs(0, Box::new(Term::Var(0)));
         assert_eq!(id, normal_form(&id, Strategy::Normal));
     }
 
     #[test]
-    fn test_reduce_id_applicative() {
+    fn test_normal_form_id_applicative() {
         let id = Term::Abs(0, Box::new(Term::Var(0)));
         assert_eq!(id, normal_form(&id, Strategy::Applicative));
     }
 
     #[test]
-    fn test_reduce_application_id_normal() {
+    fn test_normal_form_application_id_normal() {
         let term = Term::App(Box::new(Term::Abs(0, Box::new(Term::Var(0)))), Box::new(Term::Var(0)));
         assert_eq!(Term::Var(0), normal_form(&term, Strategy::Normal));
     }
 
     #[test]
-    fn test_reduce_application_id_applicative() {
+    fn test_normal_form_application_id_applicative() {
         let term = Term::App(Box::new(Term::Abs(0, Box::new(Term::Var(0)))), Box::new(Term::Var(0)));
         assert_eq!(Term::Var(0), normal_form(&term, Strategy::Applicative));
     }
 
     #[test]
-    fn test_reduce_application_to_omega_normal() {
+    fn test_nomral_form_apply_id_to_id() {
+        let id = Term::Abs(0, Box::new(Term::Var(0)));
+        assert_eq!(id, normal_form(&Term::App(Box::new(id.clone()), Box::new(id.clone())), Strategy::Normal));
+    }
+
+    #[test]
+    fn test_normal_form_application_to_omega_normal() {
         let x = Term::Abs(1, Box::new(Term::Abs(0, Box::new(Term::Var(1)))));
         let id = Term::Abs(0, Box::new(Term::Var(0)));
         let omega = Term::Abs(0, Box::new(Term::App(Box::new(Term::Var(0)), Box::new(Term::Var(0)))));
         let big_omega = Term::App(Box::new(omega.clone()), Box::new(omega));
-        let term = Term::App(Box::new(Term::App(Box::new(x), Box::new(id))), Box::new(big_omega));
+        let term = Term::App(Box::new(Term::App(Box::new(x), Box::new(id.clone()))), Box::new(big_omega));
 
-        assert_eq!(Term::Var(1), normal_form(&term, Strategy::Normal));
+        assert_eq!(id, normal_form(&term, Strategy::Normal));
     }
 
     #[test]
     #[ignore]
-    fn test_reduce_application_to_omega_applicative() {
+    fn test_normal_form_application_to_omega_applicative() {
         let x = Term::Abs(1, Box::new(Term::Abs(0, Box::new(Term::Var(1)))));
         let id = Term::Abs(0, Box::new(Term::Var(0)));
         let omega = Term::Abs(0, Box::new(Term::App(Box::new(Term::Var(0)), Box::new(Term::Var(0)))));
         let big_omega = Term::App(Box::new(omega.clone()), Box::new(omega));
-        let term = Term::App(Box::new(Term::App(Box::new(x), Box::new(id))), Box::new(big_omega));
+        let term = Term::App(Box::new(Term::App(Box::new(x), Box::new(id.clone()))), Box::new(big_omega));
 
-        assert_eq!(Term::Var(1), normal_form(&term, Strategy::Applicative));
+        assert_eq!(id, normal_form(&term, Strategy::Applicative));
     }
 }
