c937c4fbb6e34ce52ba2855284c4e2541714c527
Nikita Kamensky <me@nkamensky.com>
2019-03-13 16:02:16 +0300

    Stop mutating indices

--- bred/src/term/parser.rs
+++ bred/src/term/parser.rs
@@ -8,104 +8,82 @@ pub struct Parser {}
 
 impl Parser {
     pub fn parse(s: &str) -> Result<Term, ParseError> {
-        Self::parse_term(s, &mut 0).ok_or(ParseError {})
+        Self::parse_term(s).map(|(term, _)| { term }).ok_or(ParseError {})
     }
 
-    fn parse_term(s: &str, i: &mut usize) -> Option<Term> {
-        Some(()).skip(s, i, ' ').and_then(|_| {
-            Self::parse_app(s, i).or_else(|| {
-                Self::parse_term_in_parentheses(s, i)
+    fn parse_term(s: &str) -> Option<(Term, &str)> {
+        Some(()).and_then(|_| {
+            Self::parse_app(s).or_else(|| {
+                Self::parse_term_in_parentheses(s)
             })
         })
     }
 
-    fn parse_term_in_parentheses(s: &str, i: &mut usize) -> Option<Term> {
-        let mut index = *i;
-
-        Self::parse_symbol(s, &mut index, '(').and_then(|_| {
-            Self::parse_term(s, &mut index).and_then(|t| {
-                Self::parse_symbol(s, &mut index, ')').map(|_| {
-                    *i = index;
-                    t
+    fn parse_term_in_parentheses(s: &str) -> Option<(Term, &str)> {
+        Self::parse_symbol(s, '(').and_then(|open_parenthesis_slice| {
+            Self::parse_term(open_parenthesis_slice).and_then(|(term, term_slice)| {
+                Self::parse_symbol(term_slice, ')').map(|close_parenthesis_slice| {
+                    (term, close_parenthesis_slice)
                 })
             })
-        }).skip(s, i, ' ')
+        }).map(Self::skip_whitespace_with_value)
     }
 
-    fn parse_abs(s: &str, i: &mut usize) -> Option<Term> {
-        let mut index = *i;
-
-        Self::parse_symbol(s, &mut index, '\\').and_then(|_| {
-            Self::parse_number(s, &mut index).and_then(|v| {
-                Self::parse_symbol(s, &mut index, '.').and_then(|_| {
-                    Self::parse_term(s, &mut index).map(|t| {
-                        *i = index;
-                        Term::Abs(v, Box::new(t))
+    fn parse_abs(s: &str) -> Option<(Term, &str)> {
+        Self::parse_symbol(s, '\\').and_then(|slash_slice| {
+            Self::parse_number(slash_slice).and_then(|(var, var_slice)| {
+                Self::parse_symbol(var_slice, '.').and_then(|dot_slice| {
+                    Self::parse_term(dot_slice).map(|(term, term_slice)| {
+                        (Term::Abs(var, Box::new(term)), term_slice)
                     })
                 })
             })
-        }).skip(s, i, ' ')
+        }).map(Self::skip_whitespace_with_value)
     }
 
-    fn parse_app(s: &str, i: &mut usize) -> Option<Term> {
-        let mut index = *i;
-
-        Self::parse_sub_term(s, &mut index).map(|f| {
+    fn parse_app(s: &str) -> Option<(Term, &str)> {
+        Self::parse_sub_term(s).map(|(f, slice)| {
             let mut result = f;
+            let mut current_slice = slice;
 
-            while let Some(term) = Self::parse_sub_term(s, &mut index) {
-                result = Term::App(Box::new(result), Box::new(term))
+            while let Some((term, new_slice)) = Self::parse_sub_term(current_slice) {
+                result = Term::App(Box::new(result), Box::new(term));
+                current_slice = new_slice;
             }
 
-            result
-        }).map(|t| {
-            *i = index;
-            t
-        }).skip(s, i, ' ')
+            (result, current_slice)
+        }).map(Self::skip_whitespace_with_value)
     }
 
-    fn parse_sub_term(s: &str, mut index: &mut usize) -> Option<Term> {
-        Self::parse_var(s, &mut index).or_else(|| {
-            Self::parse_abs(s, &mut index).or_else(|| {
-                Self::parse_term_in_parentheses(s, &mut index)
+    fn parse_sub_term(s: &str) -> Option<(Term, &str)> {
+        Self::parse_var(s).or_else(|| {
+            Self::parse_abs(s).or_else(|| {
+                Self::parse_term_in_parentheses(s)
             })
         })
     }
 
-    fn parse_var(s: &str, i: &mut usize) -> Option<Term> {
-        Self::parse_number(s, i).map(|n| { Term::Var(n) }).skip(s, i, ' ')
+    fn parse_var(s: &str) -> Option<(Term, &str)> {
+        Self::parse_number(s).map(|(n, slice)| { (Term::Var(n), slice) })
     }
 
-    fn parse_symbol(s: &str, i: &mut usize, symbol: char) -> Option<()> {
-        s.chars().skip(*i).next().filter(|c| { *c == symbol }).map(|_| {
-            *i += 1;
-            ()
-        }).skip(s, i, ' ')
+    fn parse_symbol(s: &str, symbol: char) -> Option<&str> {
+        s.chars().next().filter(|c| { *c == symbol }).map(|_| { &s[1..] }).map(Self::skip_whitespace)
     }
 
-    fn parse_number(s: &str, i: &mut usize) -> Option<u32> {
-        let count = s
-            .chars()
-            .skip(*i)
-            .take_while(|c| { c.is_ascii_digit() })
-            .count();
+    fn parse_number(s: &str) -> Option<(u32, &str)> {
+        let count = s.chars().take_while(|c| { c.is_ascii_digit() }).count();
 
-        u32::from_str(&s[*i..*i + count]).ok().map(|n| {
-            *i += count;
-            n
-        }).skip(s, i, ' ')
+        u32::from_str(&s[..count]).ok().map(|n| { (n, &s[count..]) }).map(Self::skip_whitespace_with_value)
     }
-}
 
-trait Skip {
-    fn skip(self, s: &str, i: &mut usize, symbol: char) -> Self;
-}
+    fn skip_whitespace_with_value<T>((value, slice): (T, &str)) -> (T, &str) {
+        (value, Self::skip_whitespace(slice))
+    }
 
-impl<T> Skip for Option<T> {
-    fn skip(self, s: &str, i: &mut usize, symbol: char) -> Self {
-        let count = s.chars().skip(*i).take_while(|c| { *c == symbol }).count();
-        *i += count;
+    fn skip_whitespace(slice: &str) -> &str {
+        let count = slice.chars().take_while(|c| { c.is_ascii_whitespace() }).count();
 
-        self
+        &slice[count..]
     }
 }
